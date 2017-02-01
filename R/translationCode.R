fc93e671422501507a62093db300c400f2b1b7f0
stnava <stnava@gmail.com>
2017-02-01 17:06:50 -0500

    WIP: get fs matrix of data

--- R/translationCode.R
+++ R/translationCode.R
@@ -1,22 +1,35 @@
 #' Read freesurfer files into a Matrix
 #'
 #' Read freesurfer files into rows of a matrix, given a valuetype i.e. something
-#' stored in a freesurfer vertex or surface file.
+#' stored in a freesurfer vertex or surface data file.
 #'
-#' @param freesurferFileNames
-#' @param valuetype for example "thickness"
+#' @param freesurferFileNames a list of filenames pointing to freesurfer data
 #' @return A matrix containing the freesurfer data.
 #' @author Avants BB
-#' @seealso \code{\link{imagesToMatrix}, \link{getMask}}
 #' @examples
 #'
-#' # TODO
+#' if ( freesurfer::have_fs( ) ) {
+#'   fname = file.path( freesurfer::fs_subj_dir(), "bert", "surf", "lh.thickness")
+#'   thfns = c( fname, fname )
+#'   mat = freesurferToMatrix( thfns )
+#'   }
 #'
 #' @export freesurferToMatrix
-freesurferToMatrix <- function( freesurferFileNames, valuetype='thickness' ) {
+freesurferToMatrix <- function( freesurferFileNames ) {
   n <- length( freesurferFileNames )
   if (n < 1) {
     print(" length of input list must be >= 1 ")
     return(NA)
   }
+  temp = freesurfer::surf_convert( freesurferFileNames[ 1 ] )
+  ncols = ncol( temp )
+  nrows = nrow( temp )
+  mat = matrix( nrow = n, ncol = nrows )
+  for ( k in 1:length( freesurferFileNames ) ) {
+    temp = freesurfer::surf_convert( freesurferFileNames[ k ] )
+    if ( nrow( temp ) != nrows )
+      stop( paste( "for entry", k, "nrow( temp ) != nrows" ) )
+    mat[ k, ] = temp[ , ncols ]
+  }
+  return( mat )
 }
